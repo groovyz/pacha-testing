0b4f9d4608dc944d034f80481d78622f2999b1b9
groovyz <zhao@offgroove.com>
2023-09-24 14:32:45 -0400

    docker initial config

--- function_app.py
+++ function_app.py
@@ -1,10 +1,20 @@
 import azure.functions as func
 import logging
+from azure.storage.blob import BlobServiceClient
+import azure.functions as func
+import json
+import time
+from requests import get, post
+import os
+import requests
+from collections import OrderedDict
+import numpy as np
+import pandas as pd
 
 app = func.FunctionApp()
 
 @app.blob_trigger(arg_name="myblob", path="test/{name}",
-                               connection="PachafilesConnectionString") 
+                               connection="AzureWebJobsStorage") 
 def prototype_blob_trigger(myblob: func.InputStream):
     logging.info(f"Python blob trigger function processed blob"
                 f"Name: {myblob.name}"
