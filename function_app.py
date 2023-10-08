a52095bc37bb8b68f2af80b706717bbfcdc9e7cc
groovyz <zhao@offgroove.com>
2023-10-08 17:41:25 +0000

    added second function

--- function_app.py
+++ function_app.py
@@ -8,12 +8,12 @@ import insert_to_db
 
 app = func.FunctionApp()
 
-@app.blob_trigger(arg_name="myblob", path="test/{name}",
+@app.blob_trigger(arg_name="myblob", path="test/input-{name}",
                                connection="AzureWebJobsStorage")
  
 def prototype_blob_trigger(myblob: func.InputStream):
 
-    logging.info(f"Python blob trigger function processed blob"
+    logging.info(f"Python blob trigger input documents function processed blob"
                 f"Name: {myblob.name}"
                 f"Blob Size: {myblob.length} bytes")
 
@@ -22,3 +22,12 @@ def prototype_blob_trigger(myblob: func.InputStream):
     qas = analyze_doc.get_questions_answers_from(results["analyzeResult"]["content"])
     embedded_qas = analyze_doc.get_qa_with_embeddings_from(qas)
     insert_to_db.insert_into_database(embedded_qas, myblob.uri)
+
+
+
+@app.blob_trigger(arg_name="askblob", path="test/ask-{name}",
+                               connection="AzureWebJobsStorage") 
+def prototype_input_trigger(askblob: func.InputStream):
+    logging.info(f"Python blob trigger ask function processed blob"
+                f"Name: {askblob.name}"
+                f"Blob Size: {askblob.length} bytes")
